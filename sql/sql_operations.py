ee0325ddc2c02871b077410740eaa5745f404889
zhuqixing007 <zhuqixing007@gmail.com>
2019-07-14 21:30:58 +0800

    '714'

--- sql/sql_operations.py
+++ sql/sql_operations.py
@@ -4,35 +4,21 @@ import pymysql
 import datetime
 import time
 
-def insert(table, value):
-    db = pymysql.connect("localhost", "root", "123", "car")
+
+def insert_into_sensor_data(values):
+    """接受参数为一个元组，元组包含"""
+    db = pymysql.connect("localhost", "root", "root", "db_3s")
     cursor = db.cursor()
-    time_value = str(datetime.datetime.now())
-    # sql = 'INSERT INTO car_control_interface_data_receive(body,timestamp1)VALUES(' \
-    #       + "'" + value + "'" + ',' + "'" + time_value + "'" + ')'
-    # cursor.execute(sql)
+    try:
+        sql = "create table sensor_data(时间 varchar (20), 车速 VARCHAR (3), 温度 varchar (2), " \
+              "湿度 varchar (2), 明火 varchar (3), 烟雾 VARCHAR (3))"
+        cursor.execute(sql)
+    except pymysql.err.InternalError:
+        pass
+    sql = 'INSERT INTO sensor_data(时间, 车速, 温度, 湿度, 明火, 烟雾) VALUES (%s,%s,%s,%s,%s,%s)'
+    cursor.execute(sql, values)
     db.commit()
     db.close()
 
-def check_update():
-    file = open('/var/log/mysql/mysql.log')
-    file.seek(0, 2)
-    while True:
-        line = file.readline()
-        if not line:
-            time.sleep(0.1)
-            continue
-        yield line
 
-
-def get_changed_data():
-    log = check_update()
-    for line in log:
-        str1 = line.strip('\n')
-        if 'INSERT' in str1 and 'car_control_interface_data_send' in str1:
-            str2 = str1.split('VALUES')
-            pattern = re.compile(r'\'(.*?)\',', re.S)
-            msg = re.findall(pattern, str2[1])
-            print(msg[0])
-            return '*' + msg[0].rstrip('/') + '#'
-            #return '*' + str2[1] + '#'
+# insert_into_sensor_data(('2019-07-14 21:29', '0', '28', '32', '无', '无'))
