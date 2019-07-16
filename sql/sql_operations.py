15b2b2d84b0aa8ce4e8c5eeb4b11a771f53dc849
zhuqixing007 <zhuqixing007@gmail.com>
2019-07-16 13:29:15 +0800

    716

--- sql/sql_operations.py
+++ sql/sql_operations.py
@@ -6,7 +6,7 @@ import time
 
 
 def insert_into_sensor_data(values):
-    """接受参数为一个元组，元组包含"""
+    """接受参数为一个元组，元组元素顺序为时间，车速，温度，明火，烟雾"""
     db = pymysql.connect("localhost", "root", "root", "db_3s")
     cursor = db.cursor()
     try:
@@ -21,4 +21,18 @@ def insert_into_sensor_data(values):
     db.close()
 
 
+def consult_seneor_data():
+    db = pymysql.connect("localhost", "root", "root", "db_3s")
+    cursor = db.cursor()
+    sql = 'select * from sensor_data'
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for result in results:
+        print(result)
+    db.commit()
+    db.close()
+
+
+# insert_into_sensor_data(('2019-07-16 10:59', '2', '24', '30', '无', '无'))
+# consult_seneor_data()
 # insert_into_sensor_data(('2019-07-14 21:29', '0', '28', '32', '无', '无'))
